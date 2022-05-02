0815b8e0e40f26ba19b7a8c1874d99fca4089522
panda <jyjsweetie@gmail.com>
2022-05-02 23:50:05 +0900

    flo app week3

--- panda/udemy_android_flo_app/app/src/main/java/com/example/flo/HomeFragment.kt
+++ panda/udemy_android_flo_app/app/src/main/java/com/example/flo/HomeFragment.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    lateinit var binding: FragmentHomeBinding
+    lateinit var binding: com.example.flo.databinding.FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +23,16 @@ class HomeFragment : Fragment() {
         binding.homeAlbumImgIv1.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
         }
+
+        val bannerAdapter  = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+
 
         return binding.root
     }
